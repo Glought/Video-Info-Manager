a77c307dab3865be0c1e1620938438243dd28e70
Glought <thoughtsofblought@gmail.com>
2015-05-16 06:25:16 -0700

    *Deleted extra LICENSE.txt file.

--- Video Info Manager/Program.cs
+++ Video Info Manager/Program.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*Video Info Manager for easy storage and copying of Video Descriptions and Tags.
+  Copyright 2015 ThoughtsOfGlought
+
+  This file is part of Video Info Manager.
+  Video Info Manager is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Video Info Manager is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Video Info Manager.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Windows.Forms;
 
 namespace Video_Info_Manager
--- Video Info Manager/VideoInfo.cs
+++ Video Info Manager/VideoInfo.cs
@@ -1,4 +1,22 @@
-﻿namespace Video_Info_Manager
+﻿/*Video Info Manager for easy storage and copying of Video Descriptions and Tags.
+  Copyright 2015 ThoughtsOfGlought
+
+  This file is part of Video Info Manager.
+  Video Info Manager is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Video Info Manager is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Video Info Manager.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Video_Info_Manager
 {
     public class VideoInfo
     {
@@ -8,11 +26,9 @@
 
         public string Description { get; set; }
 
-
         public override string ToString()
         {
             return Name;
         }
-
     }
 }
